dd3efdfcbb8e4d0b8c45f4bad36984f45dbfd130
Thanh Nguyen <thanhduy421@gmail.com>
2024-09-21 09:23:59 +0000

    05-loremipsum: coding css

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,53 @@
+import { useState } from "react";
+import data from "./data";
+import { nanoid } from "nanoid";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleChange = (e) => {
+    //const newCount = Math.max(1,Math.min(8,e.target.value));
+    const newCount = e.target.value;
+    setCount(newCount);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setText(data.slice(0,count));
+  }
+
+  return (<section className="section-center">
+          <h4>tired of boring lorem ipsum?</h4>
+          <form onSubmit={handleSubmit} className="lorem-form">
+            <label htmlFor="amount">
+              paraghraphs:
+            </label>
+            <input 
+              type="number" 
+              onChange={handleChange}
+              value={count}
+              min = "1"
+              max = "8"
+              step = "1"
+              name = "amount"
+              id = "amount"
+              />
+            <button className="btn" type="submit">generate</button>
+          </form>
+          <article className="lorem-text">
+            {text.map((paragraph) => {
+              const id = nanoid();
+              // console.log(id);
+              return (
+                <p key={id}>
+                  {paragraph}
+                </p>
+              )
+            })
+            }
+         </article>
+        </section>);
 };
 export default App;
